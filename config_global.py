0b51c3ed55c59ea1b2020d0cc912386a654a2605
Pushkin Nikolay <adwedelf@gmail.com>
2025-05-17 20:19:46 +0700

    adsf

--- config_global.py
+++ config_global.py
@@ -3,9 +3,6 @@ from config.config import BotConfig
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from db_handler import db_class
 
--- utils/status_normalization.py
+++ utils/status_normalization.py
@@ -2,7 +2,7 @@ async def get_normal_status_name(status_name):
     """
     Перевод стадий на русский.
     """
-    from create_bot import cache_manager
+    from aiogram_run import cache_manager
 
     deal_categories = await cache_manager.get_deal_categories()
 
